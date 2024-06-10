951c57e9f3160ef80bffcea2aa6eed300b9cff7f
Yasser Abdulah Ramadhan <yasseracer1@gmail.com>
2024-06-10 09:13:48 +0700

    Mnegubah tampilan login

--- app/src/main/java/org/d3if3130/unspoken/ui/screen/LoginScreen.kt
+++ app/src/main/java/org/d3if3130/unspoken/ui/screen/LoginScreen.kt
@@ -1,50 +1,30 @@
 package org.d3if3130.unspoken.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.auth.FirebaseAuth
-import org.d3if3130.mobpro1.navigation.Screen
-import org.d3if3130.unspoken.R
 import org.d3if3130.unspoken.google.GoogleSignIn
 import org.d3if3130.unspoken.ui.theme.Orange
 import org.d3if3130.unspoken.ui.theme.UnspokenTheme
 
 @Composable
 fun LoginScreen(navController: NavHostController, googleSignIn: GoogleSignIn){
-
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
 
     Column (
         modifier = Modifier.fillMaxSize(),
@@ -80,71 +60,39 @@ fun LoginScreen(navController: NavHostController, googleSignIn: GoogleSignIn){
             modifier = Modifier.height(16.dp)
         )
 
-        OutlinedTextField(
-            value = email,
-            onValueChange = {
-                email = it
-            },
-            label = { Text(text = "Alamat Email") }
-        )
-
         Spacer(
             modifier = Modifier.height(16.dp)
         )
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            label = {
-                Text(text = "Password")
-            },
-            visualTransformation = PasswordVisualTransformation()
-        )
-
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
-
-        Button(
-            onClick = {
-                navController.navigate(Screen.Home.route)
-            }
-        ) {
-            Text(text = "Login")
-        }
         Button(onClick = { googleSignIn.onSignIn(navController) }) {
             Text(text = "Login with Google")
         }
-        IconButton(onClick = {
-            FirebaseAuth.getInstance().signOut()
-            navController.navigate(Screen.Login.route) {
-                popUpTo(Screen.Home.route) { inclusive = true }
-            }
-        }) {
-            Icon(
-                imageVector = Icons.Filled.AccountCircle,
-                contentDescription = stringResource(id = R.string.sign_out),
-                tint = androidx.compose.ui.graphics.Color.White
-            )
-        }
-        Spacer(
-            modifier = Modifier.height(32.dp)
-        )
 
-        Text(text = "Belum punya akun?", modifier = Modifier.clickable { }
-        )
-        TextButton(
-            onClick = {
-                navController.navigate(Screen.Register.route)
-            }
-        ) {
-            Text(
-                text = stringResource(id = R.string.register),
-                fontSize = 18.sp
-            )
-        }
+//        Button(
+//            onClick = {
+//                navController.navigate(Screen.Home.route)
+//            }
+//        ) {
+//            Text(text = "Login")
+//        }
+
+
+//        Spacer(
+//            modifier = Modifier.height(32.dp)
+//        )
+//
+//        Text(text = "Belum punya akun?", modifier = Modifier.clickable { }
+//        )
+//        TextButton(
+//            onClick = {
+//                navController.navigate(Screen.Register.route)
+//            }
+//        ) {
+//            Text(
+//                text = stringResource(id = R.string.register),
+//                fontSize = 18.sp
+//            )
+//        }
     }
 }
 
@@ -154,6 +102,5 @@ fun LoginScreen(navController: NavHostController, googleSignIn: GoogleSignIn){
 @Composable
 fun LoginScreenPreview() {
     UnspokenTheme {
-        LoginScreen(rememberNavController(), googleSignIn = GoogleSignIn(LocalContext.current, rememberCoroutineScope()))
-    }
+        LoginScreen(rememberNavController(), googleSignIn = GoogleSignIn(LocalContext.current, rememberCoroutineScope()))}
 }
